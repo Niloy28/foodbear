2aec51d3014fbe878f3295df26b6682d403af1e1
Farhan Ishrak Islam <niloy28@live.com>
2023-03-13 12:54:32 +0600

    Added app logo, fixed title.

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -3,10 +3,15 @@ import CartButton from "../Cart/CartButton";
 
 import styles from "../../styles/Layout/Header.module.css";
 
+import LogoImage from "../../assets/logo.png";
+
 const Header: React.FC<{ onCartButtonClick: () => void }> = (props) => {
 	return (
 		<header className={styles.header}>
-			<h1>foodbear</h1>
+			<h1>
+				{" "}
+				<img src={LogoImage} alt="" height={90} /> foodbear
+			</h1>
 			<CartButton onClick={props.onCartButtonClick} />
 		</header>
 	);
