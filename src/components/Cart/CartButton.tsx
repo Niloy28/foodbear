3fd2a3956e6c18113c795cee90b05d74ee1c7af9
Farhan Ishrak Islam <niloy28@live.com>
2023-03-13 20:44:19 +0600

    Added CartButton animation.

--- src/components/Cart/CartButton.tsx
+++ src/components/Cart/CartButton.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import CartContext from "../../store/CartContext";
 import CartIcon from "./CartIcon";
 
@@ -10,15 +10,38 @@ interface CartButtonProps {
 
 const CartButton: React.FC<CartButtonProps> = (props) => {
 	const cartCtx = useContext(CartContext);
+	const [buttonIsAnimated, setButtonIsAnimated] = useState(false);
+
+	const { orders } = cartCtx;
+	useEffect(() => {
+		if (orders.length === 0) {
+			return;
+		}
+
+		setButtonIsAnimated(true);
+
+		const timer = setTimeout(() => {
+			setButtonIsAnimated(false);
+		}, 300);
+
+		return () => {
+			clearTimeout(timer);
+		};
+	}, [orders]);
 
 	return (
-		<button onClick={props.onClick} className={styles["cart-button"]}>
+		<button
+			onClick={props.onClick}
+			className={`${styles["cart-button"]} ${
+				buttonIsAnimated ? styles["bump"] : ""
+			}`}
+		>
 			<span className={styles["cart-icon"]}>
 				<CartIcon />
 			</span>
 			<span>Your Cart</span>
 			<span className={styles["cart-badge"]}>
-				{cartCtx.orders
+				{orders
 					.map((order) => order.quantity)
 					.reduce((totalAmount, quantity) => totalAmount + quantity, 0)}
 			</span>
